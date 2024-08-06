68b52d32234740d7b88b1990397a020e0757aba0
SG-Bhuvaneshwari <sg.bhuvaneshwari98@gmail.com>
2024-08-07 04:24:16 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,24 @@
+const About = () => {
+  return (
+      <main className='About'>
+          <h2>About</h2>
+          <p style={{ marginTop: "1rem" }}>Hi ! 
+          
+          <br /> <br /> 
+          
+          This is Amirtha varshini, 
+          
+          <br /> <br /> 
+          
+          i build Social media app by using react js. 
+          
+          <br /> <br /> 
+          
+          Thanks for login my app.
+
+           </p>
+      </main>
+  )
+}
+
+export default About
